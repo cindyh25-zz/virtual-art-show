ce69270aa033549cc8cc9804bbf04723f0d9d4aa
Cindy Huang <ch832@cornell.edu>
2020-05-01 13:19:16 -0400

    delete scripts

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,43 +1,5 @@
 
-function fetch_image(imgID) {
-
-  // $.get("index.php", "&img_id=" + imgID, function (imgID) {
-  //   document.getElementById("overlay-wrapper").style.display = "block";
-  // });
-
-  $.get("index.php", "&img_id=" + imgID, function (results) {
-    $("#replace").html(results);
-  }
-  );
-
-}
-
-function closeOverlay() {
-  alert("hi");
-  document.getElementById("overlay-wrapper").style.display = "none";
-}
-
-function showSideMenu() {
-  document.getElementById("sidemenu").style.display = "inline-block";
-  document.getElementById("menuicon").style.display = "none";
-}
-
-function hideSideMenu() {
-
-  document.getElementById("sidemenu").style.display = "none";
-  document.getElementById("menuicon").style.display = "block";
-}
 
 function showSettings() {
   document.getElementById("settingscontent").style.display = "inline-block";
-}
-
-function showModal(modal) {
-  document.getElementById(modal).style.display = "block";
-  document.getElementById(modal).style.zindex = "500";
-  document.getElementById('settingscontent').style.display = "none";
-}
-
-function closeModal(modal) {
-  document.getElementById(modal).style.display = "none";
 }
